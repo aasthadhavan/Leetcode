a55bac7e625bf06f3ca1a2c2ba8baaf76ebc78eb
Aastha Dhavan <aasthadhavan@gmail.com>
2026-05-30 21:40:30 +0530

    Day 77

---
+++ Medium/DecodeString.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> n=new Stack<>(); //number stack
+        Stack<String> m=new Stack<>(); //string stack
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(ch>='0' && ch<='9'){
+                int num=0;
+                while(i<s.length() && s.charAt(i)>='0' && s.charAt(i)<='9'){
+                    num=num*10+(int)(s.charAt(i)-'0');
+                    i++;
+                }
+                i--;
+                n.push(num);
+
+            } else if(ch!=']'){
+                m.push(ch+"");
+            }
+            else{
+                String str="";
+                while(!m.peek().equals("[")){
+                    str=m.pop()+str;
+                }
+                m.pop();
+                int repetition=n.pop();
+
+                StringBuilder sb=new StringBuilder("");
+                while(repetition>0){
+                    sb.append(str);
+                    repetition--;
+                }
+                m.push(sb.toString());
+
+            }
+        }
+        StringBuilder ans=new StringBuilder("");
+        while(m.size()>0){
+            ans.insert(0,m.pop());
+        }
+        return ans.toString();
+    }
+}

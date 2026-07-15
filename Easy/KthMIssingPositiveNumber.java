4bd0a1c137a211f0c0d07dbfb4cf48adeb532402
Aastha Dhavan <aasthadhavan@gmail.com>
2026-07-15 23:55:38 +0530

    Implement Kth Missing Positive Number solution

---
+++ Easy/KthMIssingPositiveNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int l=0;
+        int h=arr.length-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                l=mid+1;
+            } else{
+                h=mid-1;
+            }
+        }
+      return h+k+1; 
+    }
+}

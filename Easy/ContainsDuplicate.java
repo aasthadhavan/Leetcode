cdd0d7dda82232dfa57b3016dba78169c1c1ba9f
Aastha Dhavan <aasthadhavan@gmail.com>
2026-07-26 20:56:34 +0530

    Create ContainsDuplicate.java

---
+++ Easy/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int n:nums){
+            if(!hs.add(n)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
